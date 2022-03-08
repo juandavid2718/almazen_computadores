b96ab170f7c08081d18f9f9ab8d2a0bf37a20c96
juandavid2718 <juandavid5b@hotmail.com>
2022-03-08 00:33:24 -0500

    se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,16 @@
+let boton = document.getElementById('miboton');
+boton.addEventListener('click', function (){
+let input = document.getElementById("cantidad").value;
+console.log (cantidad)
+resultado = input * 820000;
+let text = document.getElementById("respuesta");
+text.value=resultado;
+
+
+
+
+});
+
+alert("hola")
+
+ 
